cc06f6cb92364b22d2631e366323f905bd7b793e
Michaela Davis and Stella Huayhuaca <stella@>
2017-01-18 16:40:09 -0800

    add ay to end of words beginning with vowels

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
-
+// Business logic
 var vowels = ["a", "e", "i", "o", "u"];
-var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t","v","x","y","z"];
+var consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s", "t","v","x","y","z"];
 
 var translate = function(word) {
-
-  if (word && word.length != 0) {
-    return word;
-  }else {
-     alert("enter a word");
+  // if character at index 0 of your word is NOT included in consonants then return word
+  if (!consonants.includes(word.charAt(0))) {
+    return word + "ay";
+  // } else if ((word.length !== 0) && (word !== " ")) {
+  //   alert("first");
+  //   return word;
+   } else {
+     alert("This is not a word. Please enter one.");
   }
 }
 
-
+// User Interface Logic
 $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
