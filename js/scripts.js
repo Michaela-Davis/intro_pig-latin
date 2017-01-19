4d766d1962861a33d9133f2aa4ede10af90d2309
Michaela-Davis <michaela.delight@gmail.com>
2017-01-18 21:33:24 -0800

    business logic REDO

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,38 @@
 // Business logic
 var vowels = ["a", "e", "i", "o", "u"];
-var consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s", "t","v","x","y","z"];
+var consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","x","y","z"];
 
-var translate = function(word) {
-  // if character at index 0 of your word is NOT included in consonants then return word
-  if (!consonants.includes(word.charAt(0))) {
-    return word + "ay";
-  // } else if ((word.length !== 0) && (word !== " ")) {
-  //   alert("first");
-  //   return word;
-   } else {
-     alert("This is not a word. Please enter one.");
+
+var firstVowel = function(word) {
+  var firstLetter = word.charAt(0).toLowerCase();
+  // if (vowels.includes(firstLetter)) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  //   !!!!! IS THE SAME AS:
+  // return vowels.includes(firstLetter);
+  return vowels.includes(firstLetter);
+};
+
+var translateWord = function(word) {
+  // check for input
+  if (word.length === 0) {
+    alert("You must enter a word");
   }
-}
+  if (firstVowel(word)) {
+    alert("the first letter is a vowel");
+  }
+
+};
 
 // User Interface Logic
 $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
 
-    var word=  $("input#translate").val();
-    var result= translate(word);
-
+    var word =  $("input#translate").val();
+    var result = translateWord(word);
 
     $("#output").text(result);
   });
